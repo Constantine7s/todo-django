8ab613855327f86c559e9e87e013e1b24c22714a
Konstantin Shambazov <k.shambazov@gmail.com>
2022-07-31 23:46:34 +0900

    feat: add single task

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, SingleTask
 
 urlpatterns = [
     path('',TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>',SingleTask.as_view(), name='task'),
+
 ]
+
--- base/views.py
+++ base/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from base.models import Task
 
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
+
+class SingleTask(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/single_task.html'
