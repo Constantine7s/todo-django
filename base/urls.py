f85b48837b95461fcd7d2ff0ea1755c7b9794cbe
Konstantin Shambazov <k.shambazov@gmail.com>
2022-08-01 12:40:39 +0900

    feat: add create task functionality

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import TaskList, SingleTask
+from .views import TaskCreate, TaskList, SingleTask
 
 urlpatterns = [
     path('',TaskList.as_view(), name='tasks'),
     path('task/<int:pk>',SingleTask.as_view(), name='task'),
-
+    path('task/create/',TaskCreate.as_view(), name='task-create'),
 ]
 
